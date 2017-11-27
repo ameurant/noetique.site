1b66e7ec04723c26c10f35bb1b8500ab0b22484f
Alain Meurant <alain.meurant@skynet.be>
2017-11-27 21:01:18 +0100

    modification bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,8 @@
 rm -r ./lib ./include ./bin
 ln -fs base.cfg buildout.cfg
 virtualenv-2.7 --clear .
-./bin/pip install zc.buildout
+
+./bin/pip install -U "setuptools==21.0.0"
+./bin/pip install -U pip
+./bin/pip install -U "zc.buildout==2.5.2"
 ./bin/buildout

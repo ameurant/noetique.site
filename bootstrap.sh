d23417339644286aaf4f8c2f95ecc1c15b96ee32
ameurant <alain.meurant@netvaast.be>
2020-05-12 09:58:09 +0200

    Modificar le bootstrapsh en enlevant —clear

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
-rm -r ./lib ./include ./bin
 ln -fs base.cfg buildout.cfg
-virtualenv-2.7 --clear .
+virtualenv-2.7 .
 
 ./bin/pip install -U "zc.buildout==2.5.3"
 ./bin/pip install -U "setuptools==26.1.1"
+
 ./bin/buildout
